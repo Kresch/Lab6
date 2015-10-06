69afcc1d9de0c89718c55e60a32862ae53fdf899
Kresch <niclas.lovsjo@me.com>
2015-10-06 23:57:53 +0200

    vignetted some

--- Lab6/vignettes/lab_report_knapsack.R
+++ Lab6/vignettes/lab_report_knapsack.R
@@ -1,19 +1,20 @@
-## ----echo=FALSE----------------------------------------------------------
+## ------------------------------------------------------------------------
+set.seed(42)
 n <- 16
 knapsack_objects <-
-data.frame(
-w=sample(1:4000, size = n, replace = TRUE),
-v=runif(n = n, 0, 10000)
-)
-iter=0
-times<-c()
-while (iter<1000){
-  time<-proc.time()
-  brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
-  times<-c(times,proc.time()-time)
-  iter<-iter+1
-}
+        data.frame(
+                w=sample(1:4000, size = n, replace = TRUE),
+                v=runif(n = n, 0, 10000)
+        )
+system.time(brute_force_knapsack(knapsack_objects,2000))
 
 ## ------------------------------------------------------------------------
-mean(times)
+set.seed(42)
+n <- 500
+knapsack_objects <-
+        data.frame(
+                w=sample(1:4000, size = n, replace = TRUE),
+                v=runif(n = n, 0, 10000)
+        )
+system.time(knapsack_dynamic(knapsack_objects,2000))
 
